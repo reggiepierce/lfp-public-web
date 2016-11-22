1deec134a6f534f92d96dc2c304e9a6ba603cd1c
reggiepierce <reggie.pierce@iplasso.com>
2016-11-21 21:19:39 -0500

    Create javascript-utils.js

---
+++ js/javascript-utils.js
@@ -0,0 +1,19 @@
+function getParents(el, parentSelector /* optional */) {
+
+    // If no parentSelector defined will bubble up all the way to *document*
+    if (parentSelector === undefined) {
+        parentSelector = document;
+    }
+
+    var parents = [];
+    var p = el.parentNode;
+
+    while (p !== parentSelector) {
+        var o = p;
+        parents.push(o);
+        p = o.parentNode;
+    }
+    parents.push(parentSelector); // Push that parentSelector you wanted to stop at
+
+    return parents;
+}
